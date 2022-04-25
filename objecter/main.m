3cf732185430fb2caf7d6d1b3f6bdb991a57e9e3
Misha Causur <mishacausur@mail.ru>
2022-04-25 23:41:20 +0500

    person is implement

--- objecter/main.m
+++ objecter/main.m
@@ -4,8 +4,17 @@
 //
 //  Created by Misha Causur on 25.04.2022.
 //
-
+#import <stdlib.h>
+#import <stdio.h>
+#import <string.h>
 #import <Foundation/Foundation.h>
+
+typedef struct {
+    char *name;
+    size_t age;
+} person_t;
+
+void login_user(person_t *user);
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
