a1f261065a81ed80ec8df726c9cf1cf4ad173718
Misha Causur <mishacausur@mail.ru>
2022-04-25 23:37:09 +0500

    Initial Commit

---
+++ objecter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  objecter
+//
+//  Created by Misha Causur on 25.04.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
